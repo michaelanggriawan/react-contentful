1b20a868061cc1ba044c874f78f2e2c289c1db80
michaelanggriawan <michael@oddle.me>
2021-05-12 22:51:21 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ['images.ctfassets.net']
+  },
+  env: {
+    SPACE: '7r3uot39bhra',
+    ACESS_TOKEN: 'XrVJv9PDJYcbKz327IQcZofxjySS9NilImYE0uD49No'
+  }
+}
